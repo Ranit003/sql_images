02af9ae94e014dc85f999401c685b966cad21ca8
Ranit003 <ranitsharma121@gmail.com>
2024-10-01 17:38:02 +0530

    WORKING WITH IIMAGES

---
+++ android/app/src/main/kotlin/com/example/images_sql/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.images_sql
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController caption=TextEditingController();
+  File? imagepath;
+  String? imagename;
+  String? imagedata;
+
+  ImagePicker imagePicker=new ImagePicker();
+  Future<void> uploadimage() async{
+    try{
+      String uri = "http://10.0.2.2/uploadimage/uploadimage.php";
+      var res=await http.post(Uri.parse(uri),body: {
+        "caption":caption.text,
+        "data":imagedata,
+        "name":imagename,
+      });
+      var response=jsonDecode(res.body);
+      if(response["success"]=="true"){
+        print("uploaded");
+      }
+      else{
+        print("some issue");
+      }
+    }
+    catch(e) {
+      print(e);
+    }
+  }
+  Future<void> getImage() async{
+    var getimage=await imagePicker.pickImage(source:ImageSource.gallery);
+    setState(() {
+      imagepath=File(getimage!.path);
+      imagename=getimage.path.split('/').last;
+      imagedata=base64Encode(imagepath!.readAsBytesSync());
+      print(imagepath);
+      print(imagename);
+      print(imagedata);
+    });
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Upload Image"),
+        ),
+        body: Column(
+          children: [
+            SizedBox(height: 20,),
+            TextFormField(
+              decoration: InputDecoration(border: OutlineInputBorder(
+              ),
+              label: Text("Enter the Caption"),
+              ),
+            ),
+            SizedBox(height: 20,),
+            imagepath != null
+                ? Image.file(imagepath!)
+                : Text("Image not chosen yet"),
+            SizedBox(height: 20,),
+            ElevatedButton(onPressed: (){
+              getImage();
+            }, child: Text("Choose Image")),
+            ElevatedButton(onPressed: (){
+              setState(() {
+                uploadimage();
+              });
+            }, child: Text("Upload")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const MyApp());
+}
